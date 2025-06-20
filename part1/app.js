db98ea52e32b4afc98a4fd131a40b4a5b1e9ffe7
Marko Bogosavljevic <markobogosavljevic06@gmail.com>
2025-06-20 09:07:04 +0000

    [automatic commit] 2025-06-20 09:07:04 UTC 129.127.202.25

--- part1/app.js
+++ part1/app.js
@@ -22,18 +22,12 @@ let db;
 
 (async () => {
   try {
-    // Connect to MySQL without specifying a database
     const connection = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
       password: ''
     });
 
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
-    await connection.end();
-
-    // Now connect to the created database
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
