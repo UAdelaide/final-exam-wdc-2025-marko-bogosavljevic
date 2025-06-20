674ad5a4b98989d17d170c3a2ae700e3daee7e1c
Marko Bogosavljevic <markobogosavljevic06@gmail.com>
2025-06-20 09:18:30 +0000

    [automatic commit] 2025-06-20 09:18:30 UTC 129.127.202.25

--- part1/app.js
+++ part1/app.js
@@ -51,7 +51,7 @@ app.get('/api/dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
-    const [walkrequests] = await db.execute('SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests;');
+    const [walkrequests] = await db.execute('SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location;');
     res.json(walkrequests);
   } catch (err) {
     res.status(500).json({ error: 'failed' });
