a7709c2fcb222b9ac7bebe8bb14e0c9445e36654
Marko Bogosavljevic <markobogosavljevic06@gmail.com>
2025-06-20 11:04:11 +0000

    [automatic commit] 2025-06-20 11:04:11 UTC 129.127.202.82

--- part1/app.js
+++ part1/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
-var mysql = require('mysql');
+var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
