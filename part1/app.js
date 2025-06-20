a3d01a78595101a5595d19cdff77db58685c16c6
Marko Bogosavljevic <markobogosavljevic06@gmail.com>
2025-06-20 09:07:45 +0000

    [automatic commit] 2025-06-20 09:07:45 UTC 129.127.202.25

--- part1/app.js
+++ part1/app.js
@@ -41,9 +41,9 @@ let db;
 })();
 
 // Route to return books as JSON
-app.get('/', async (req, res) => {
+app.get('/api/dogs', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT * FROM books');
+    const [books] = await db.execute('SELECT Dogs,.');
     res.json(books);
   } catch (err) {
     res.status(500).json({ error: 'failed' });
