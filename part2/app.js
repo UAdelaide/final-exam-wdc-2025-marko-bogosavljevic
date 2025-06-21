e619170c16e5f69f1637029c42102e4508df67b4
Marko Bogosavljevic <markobogosavljevic06@gmail.com>
2025-06-21 03:12:14 +0000

    [automatic commit] 2025-06-21 03:12:14 UTC 129.127.202.82

--- part2/app.js
+++ part2/app.js
@@ -16,8 +16,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         secure: false, // true if using https
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000 // 1 day
+        maxAge: 24 * 60 * 60 * 1000
     }
 }));
 
