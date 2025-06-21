8c12beba6648da98a3d8bc339ae11079343183bf
Marko Bogosavljevic <markobogosavljevic06@gmail.com>
2025-06-21 03:16:21 +0000

    [automatic commit] 2025-06-21 03:16:21 UTC 129.127.202.82

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,7 +50,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
     console.log(rows);
-    res.json({ message: 'Login successful', user: rows });
+    res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
