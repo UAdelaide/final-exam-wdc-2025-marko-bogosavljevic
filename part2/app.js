a7662dc8053d128649eb9c0345e1d2ac1537f858
Marko Bogosavljevic <markobogosavljevic06@gmail.com>
2025-06-21 03:11:13 +0000

    [automatic commit] 2025-06-21 03:11:13 UTC 129.127.202.82

--- part2/app.js
+++ part2/app.js
@@ -2,17 +2,19 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const session = require('express-session');
-const cookie = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(cookieParser());
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const cookieParser = require('cookie-parser');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
