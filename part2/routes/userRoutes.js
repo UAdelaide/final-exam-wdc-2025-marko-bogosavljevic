969522417a0fbd13ad0393ed51a5faafdc7d55b4
Marko Bogosavljevic <markobogosavljevic06@gmail.com>
2025-06-21 02:26:23 +0000

    [automatic commit] 2025-06-21 02:26:23 UTC 129.127.202.82

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,6 +37,7 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
+  // const { email, password } = req.body;
   const { user, pass } = req.body;
 
   try {
